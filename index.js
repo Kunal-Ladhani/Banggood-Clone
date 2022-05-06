042f668fea32774473b4076d4543cc71c336f959
Kunal Ladhani <k.ladhani1@gmail.com>
2022-05-07 00:36:03 +0530

    files edited

---
+++ index.js
@@ -0,0 +1,291 @@
+mensData = [
+    {
+        id:"1535905",
+        name : "Men 100% Cotton Ethnic Pattern Patchwork Dual Pockets Vintage Shirts - Navy M",
+        image_url : "https://imgaz.staticbg.com/thumb/large/oaupload/banggood/images/52/D1/7b85b464-02ea-4b66-a73e-7c55ac1fea77.jpg.webp",
+        category:"Men's Clothing", 
+        price:"$14.99",
+        strickedoffprice:"$20",
+    },
+    {
+        id:"1705238",
+        name : "Cotton Galaxy Print Round Neck Casual Short Sleeve T-Shirts - Black XL",
+        image_url : "https://imgaz3.staticbg.com/thumb/large/oaupload/banggood/images/CA/5F/50e614f6-8f77-4709-b5cf-c2109b7f8477.jpeg.webp",
+        category:"Men's Clothing",
+        price:"$9.99",
+        strickedoffprice:"$29.99",
+    },
+    {
+        id:"1703941",
+        name : "Mens Print Cotton Casual Round Neck Short Sleeve T-Shirts - Pink 3XL",
+        image_url : "https://imgaz3.staticbg.com/thumb/large/oaupload/banggood/images/25/44/baaf1127-5f1c-4ac8-9cf6-0e28d77824ef.jpeg.webp",
+        category:"Men's Clothing",
+        price:"$10.99",
+        strickedoffprice:"$27.99",
+    },
+    {
+        id:"1713619",
+        name : "Mens Thin Corduroy Mustard Set Patch Pocket Breathable Short Sleeve Shirt & Shorts - Blue S",
+        image_url : "https://imgaz2.staticbg.com/thumb/large/oaupload/banggood/images/72/CB/281dee5d-1e85-4028-abbf-d44f885bba2a.jpeg.webp",
+        category:"Men's Clothing", 
+        price:"",
+        strickedoffprice:"",
+    },
+    {
+        id:"1840684",
+        name : "Mens Cartoon Colorful Mushroom Print Lapel Short Sleeve Shirt - Black S",
+        image_url : "https://imgaz1.staticbg.com/thumb/large/oaupload/banggood/images/F4/13/c7b26f99-c6fa-4090-b25a-70184f1b2749.jpg.webp",
+        category:"Men's Clothing",
+        price:"$19.99",
+        strickedoffprice:"$28.56",
+    },
+    {
+        id:"1750124",
+        name : "Mens Cotton Stand Collar Multi Pocket Zipper Long Sleeve Simple Jacket - Khaki S",
+        image_url : "https://imgaz1.staticbg.com/thumb/large/oaupload/banggood/images/22/B5/fe91e034-6831-4be5-90fc-c402372e47eb.jpg.webp",
+        category:"Men's Clothing",
+        price:"$53.99",
+        strickedoffprice:"$77.15",
+    },
+    {
+        id:"1917163",
+        name : "Men Corduroy Patchwork Contrast Color Block Lapel Casual Shirts - Green S",
+        image_url : "https://imgaz.staticbg.com/thumb/large/oaupload/banggood/images/23/D2/2e87dad7-85dc-4602-81dc-cd3caa902e70.jpg.webp",
+        category:"Men's Clothing",
+        price:"$30.99",
+        strickedoffprice:"$44.27",
+    },
+    {
+        id:"1699705",
+        name : "Men Cotton Solid Color Little Tag Drawstring Casual Shorts With Pocket - White M",
+        image_url : "https://imgaz2.staticbg.com/thumb/large/oaupload/banggood/images/0B/94/7074eb1f-56c7-49da-a698-24efe382027c.jpg.webp",
+        category:"Men's Clothing",
+        price:"$22.99",
+        strickedoffprice:"$32.86",
+    },
+    {
+        id:"1789165",
+        name : "Mens Corduroy Solid Color Casual Pants With Pocket - Black L",
+        image_url : "https://imgaz1.staticbg.com/thumb/large/oaupload/banggood/images/D4/E7/a0d0ba69-43af-4284-9d70-e7135cd6f8bd.jpg.webp",
+        category:"Men's Clothing",
+        price:"$46.99",
+        strickedoffprice:"$67.13",
+    },
+    {
+        id:"1825252",
+        name : "Mens Casual Flower Print Short Sleeve Shirts - White M",
+        image_url : "https://imgaz2.staticbg.com/thumb/large/oaupload/banggood/images/E9/36/65c582aa-8e68-48d5-9bfc-95b35715a80a.jpeg.webp",
+        category:"Men's Clothing",
+        price:"$27.12",
+        strickedoffprice:"$39.22",
+    },
+    {
+        id:"1527110",
+        name : "Pine Leaves Print Cotton Short Sleeve Relaxed Shirts - S Green",
+        image_url : "https://imgaz.staticbg.com/thumb/large/oaupload/ser1/banggood/images/F8/CD/ab0bdfea-eeaf-44d8-b595-85fbe91f74f8.JPG.webp",
+        category:"Men's Clothing",
+        price:"$19.99",
+        strickedoffprice:"$29.99",
+    },
+    {
+        id:"1525703",
+        name : "Men's Knit Cap Hat Padded Warm Beret Caps Casual Outdoor Visor Forward Hat - Black",
+        image_url : "https://imgaz2.staticbg.com/thumb/large/oaupload/banggood/images/8F/7D/65646787-cf59-4915-bd9a-9b8f430b618b.jpg.webp",
+        category:"Men's Clothing",
+        price:"$15.22",
+        strickedoffprice:"$45.99",
+    },
+    {
+        id:"1610723",
+        name : "Collrown Men Vintage Painter Beret Caps Octagonal Newsboy Cap Cabbie Ivy Flat Hat - Coffee L",
+        image_url : "https://imgaz1.staticbg.com/thumb/large/oaupload/banggood/images/C4/C2/b722515f-4baa-4afa-88ad-6bad0b8099d9.jpg.webp",
+        category:"Men's Clothing",
+        price:"$11.99",
+        strickedoffprice:"$38.99",
+    },
+    {
+        id:"1725948",
+        name : "Mens Corduroy Solid Color Drawstring Mid Waist Elastic Cuff Jogger Pants - Blue S",
+        image_url : "https://imgaz.staticbg.com/thumb/large/oaupload/banggood/images/55/2F/1db5b84f-c0a4-41a9-b908-03a89df1d1c5.jpg.webp",
+        category:"Men's Clothing",
+        price:"$28.99",
+        strickedoffprice:"$48.99",
+    },
+    {
+        id:"1279723",
+        name : "Charmkpr Mens Loose Long Sleeve Cotton Linen Tops Breathable Antibacterial Vintage T-Shirts - S White",
+        image_url : "https://imgaz2.staticbg.com/thumb/large/oaupload/ser1/banggood/images/1C/DF/b90dc209-d0bd-4a21-8db4-daba359c331f.jpg.webp",
+        category:"Men's Clothing",
+        price:"$25.99",
+        strickedoffprice:"$39.13",
+    }
+];
+
+// Write JS here 
+//Card One 
+var data =[
+    { imgUrl:"https://imgaz2.staticbg.com/thumb/grid/oaupload/banggood/images/40/D0/4ad9cdc1-0792-42eb-af59-c035ee52f1c1.jpg.webp",
+      price:"₹5,532.54",
+      colorPrice:"62%Off",
+    },
+    { imgUrl:"https://imgaz2.staticbg.com/thumb/grid/oaupload/banggood/images/3E/8E/8d3c66b4-83f5-42dc-863b-5a65830275c5.jpg.webp",
+    price:"₹7,532.29",
+    colorPrice:"₹55%Off",
+    },
+    { imgUrl:"https://imgaz2.staticbg.com/thumb/grid/oaupload/banggood/images/4B/C7/51465cf3-35e8-446c-a35a-4f978bd779ad.jpg.webp",
+        price:"₹15,508.11",
+        colorPrice:"43%Off",
+    },
+    { imgUrl:"https://imgaz1.staticbg.com/thumb/grid/oaupload/banggood/images/30/36/08304cbc-ab96-4423-b57d-ff49649e9967.jpg.webp",
+    price:"₹5,376.88",
+    colorPrice:"51%Off",
+    },
+    { imgUrl:"https://imgaz.staticbg.com/thumb/grid/oaupload/banggood/images/9C/B3/e8fe90f9-e578-4584-b9a8-8740814ed989.jpg.webp",
+    price:"₹20,994.52",
+    colorPrice:"30%Off",
+    },
+    { imgUrl:"https://imgaz2.staticbg.com/thumb/grid/oaupload/banggood/images/F1/71/6ea0a33e-689f-4f77-9ffe-07cf0473e686.jpg.webp",
+    price:"₹1,776.20",
+    colorPrice:"62%Off",
+    },
+    { imgUrl:"https://imgaz2.staticbg.com/thumb/grid/oaupload/banggood/images/F4/B3/53f7d537-5620-40e5-86f9-32d6614e4aaa.jpg.webp",
+    price:"₹310.96",
+    colorPrice:"67%Off",
+    },
+    { imgUrl:"https://imgaz.staticbg.com/thumb/grid/oaupload/banggood/images/35/B6/da9ab6a0-827e-4c13-8f61-0234886d227c.jpg.webp",
+    price:"₹5,454.20",
+    colorPrice:"24%Off",
+    },
+    
+]
+
+data.map(function (el){
+    var box = document.createElement("div")
+    
+
+    var prodcut =  document.createElement("img")
+    prodcut.setAttribute("src", el.imgUrl)
+  
+
+    var pro_price = document.createElement("p")
+    pro_price.innerText = el.price
+
+    var price_tag = document.createElement("p")
+    price_tag.innerText = el.colorPrice
+
+    box.append(prodcut, pro_price, price_tag)
+    document.querySelector("#container").append(box)
+})
+
+var form = document.querySelector("form").addEventListener("submit",myFunction)
+
+function myFunction(){
+    email = document.querySelector("#email").value = alert("Welcome to the Shop")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
