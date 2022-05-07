cf5ded7763e7e6995ade58eddeba574b8443abed
Kunal Ladhani <k.ladhani1@gmail.com>
2022-05-07 18:40:52 +0530

    completed content pages

--- js/homepage.js
+++ js/homepage.js
@@ -80,3 +80,11 @@
     );
   }
 
+  var counter = 1;
+  setInterval(function () {
+    document.getElementById("radio" + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+      counter = 1;
+    }
+  }, 5000);
